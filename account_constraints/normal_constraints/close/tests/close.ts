a23077dee4eabf2330d14a91b010aad1c2c988a9
MichaelWang <michael_wangguanyi@163.com>
2025-01-20 11:22:59 +0800

    strengthen the tests for 'close'

--- account_constraints/normal_constraints/close/tests/close.ts
+++ account_constraints/normal_constraints/close/tests/close.ts
@@ -36,5 +36,16 @@ describe("close", () => {
     // check the destination of the rent in the account closed
     const otherAccountInfo = await connection.getAccountInfo(other.publicKey);
     assert.equal(otherAccountInfo.lamports, accountInfo.lamports);
+
+    // check the account closed
+    assert.equal(await connection.getAccountInfo(accKey.publicKey), null);
+    try {
+      await program.account.accountData.fetch(accKey.publicKey);
+    } catch (err) {
+      assert.strictEqual(
+        err.message,
+        `Account does not exist or has no data ${accKey.publicKey}`
+      );
+    }
   });
 });
