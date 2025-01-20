3570eae6398db3e871cc51e3f861c5b966da54c6
MichaelWang <michael_wangguanyi@163.com>
2025-01-20 11:15:03 +0800

    close

---
+++ account_constraints/normal_constraints/close/programs/close/src/lib.rs
@@ -0,0 +1,48 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Agq8YHbRvpowkU3fEd4EMPMFEyfH7BiTz9kHjWCUHbP1");
+
+#[program]
+pub mod close {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, i: u64) -> Result<()> {
+        ctx.accounts.account.i = i;
+        Ok(())
+    }
+
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        msg!("Account close: {:?}", ctx.accounts.receiver);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Close<'info> {
+    #[account(mut)]
+    /// CHECK: receiver for the sol from account
+    pub receiver: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        close = receiver,
+    )]
+    pub account: Account<'info, AccountData>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 8,
+    )]
+    pub account: Account<'info, AccountData>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct AccountData {
+    i: u64,
+}
---
+++ account_constraints/normal_constraints/close/tests/close.ts
@@ -0,0 +1,40 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program, BN } from "@coral-xyz/anchor";
+import { Close } from "../target/types/close";
+import { Keypair } from "@solana/web3.js";
+import assert from "assert";
+
+describe("close", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const wallet = provider.wallet as anchor.Wallet;
+  const connection = provider.connection;
+  const program = anchor.workspace.Close as Program<Close>;
+  const accKey = new Keypair();
+  const other = new Keypair();
+
+  before(async () => {
+    await program.methods.initialize(new BN(1024))
+      .accounts({
+        signer: wallet.publicKey,
+        account: accKey.publicKey
+      })
+      .signers([accKey])
+      .rpc();
+  })
+
+  it("close the account", async () => {
+    const accountInfo = await connection.getAccountInfo(accKey.publicKey);
+    await program.methods.close()
+      .accounts({
+        account: accKey.publicKey,
+        receiver: other.publicKey,
+      })
+      .rpc();
+
+    // check the destination of the rent in the account closed
+    const otherAccountInfo = await connection.getAccountInfo(other.publicKey);
+    assert.equal(otherAccountInfo.lamports, accountInfo.lamports);
+  });
+});
